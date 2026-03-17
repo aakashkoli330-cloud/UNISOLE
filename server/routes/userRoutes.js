572a59254ff2831c35b69e8222e667f52756a7b9
aakashkoli330-cloud <aakashkoli330@gmail.com>
2026-03-17 14:31:07 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/authMiddleware");
-const User = require("../models/User");
+const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
 /* =====================
